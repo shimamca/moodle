ac50671bce719ef3489f152f9311ce868b4fabc9
Shima Kausar <shimak@chetu.com>
2017-11-29 17:07:51 +0530

    modified

--- lib.php
+++ lib.php
@@ -19,7 +19,7 @@
  *
  * @copyright 1999 Martin Dougiamas  http://dougiamas.com
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @package core_course
+ * @package core_course...........
  */
  
  
